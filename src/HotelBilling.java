0cf924fe431dee022624370ea58349a8d776a908
daly81 <idaly81@gmail.com>
2013-08-16 10:58:28 -0400

    HotelRooms Ian Daly

--- src/HotelBilling.java
+++ src/HotelBilling.java
@@ -1,21 +1,58 @@
+import javax.swing.JOptionPane;
+import java.util.Scanner; 
 
 public class HotelBilling {
 
+	final static float SALES_TAX = 0.13f; 
+	
 	public static void main(String[] args) {
-		/*
-		 * In the main method, declare and initialize variables
-		 * for room price, number of rooms booked, and coupon rate.
-		 * 
-		 * Then call each case of the overloaded method 
-		 * calculateBill() to demonstrate the use of overloaded 
-		 * methods
-		 */
+
+		double roomRate = 95.00; 
+		double couponRate = 0.10; 
+		int rooms = 3;
+		double totalBill; 
+		
+		totalBill = calculateBill(roomRate); 
+		JOptionPane.showMessageDialog(null, "Your total bill with tax comes to: " + totalBill); 
+		
+		totalBill = calculateBill(roomRate, rooms); 
+		JOptionPane.showMessageDialog(null, "Your total with tax for " + rooms + " room(s) is $ " + totalBill);
+		
+		totalBill = calculateBill(roomRate, rooms, couponRate); 
+		JOptionPane.showMessageDialog(null, "Your total with tax for " + rooms + " room(s) is $ " + totalBill 
+				+ " This includes a discount of " + couponRate*100 + "%"); 
+		
+		
+	}
+	
+	public static double calculateBill(double rate, int rooms, double coupon) {
+		double tax = rate * SALES_TAX; 
+		double finalRate = rate + tax; 
+		finalRate = finalRate * rooms * (1 - coupon); 
+		finalRate = twoDecimalRounding(finalRate); 
+		return finalRate; 
+	}
+	
+	public static double calculateBill(double rate, double rooms) {
+		double tax = rate * SALES_TAX; 
+		double finalRate = rate + tax; 
+		finalRate = finalRate * rooms;  
+		finalRate = twoDecimalRounding(finalRate); 
+		return finalRate; 
+	}
+	
+	public static double calculateBill(double rate) {
+		double tax = rate * SALES_TAX; 
+		double finalRate = rate + tax; 
+		finalRate = twoDecimalRounding(finalRate); 
+		return finalRate; 
 	}
 
-	/*  
-	 * Here you will create four new methods:
-	 * -- Three cases of the overloaded method calculateBill()
-	 * -- A method called twoDecimalRounding() which will
-	 * 		be called by each case of the calculateBill() method.
-	 */
+	private static double twoDecimalRounding(double decimal) {
+		decimal = decimal*100; 
+		decimal = decimal + 0.5; 
+		decimal = (int) decimal; 
+		decimal = decimal/100; 
+		return decimal;
+	}
 }
